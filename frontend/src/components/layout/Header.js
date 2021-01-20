ade87cc4591113ca9ded7f549d0fb9a6bb25c639
Warrior1119 <warriorjhs@outlook.com>
2021-01-20 10:23:03 -0800

    update

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -6,6 +6,7 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
 import AppContext from "../AppContext";
+import styles from './Header.module.scss';
 
 const Header = ({ history }) => {
   const appContext = useContext(AppContext);
@@ -27,55 +28,75 @@ const Header = ({ history }) => {
 
 
   return (
-    <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
-      <Navbar.Brand as={Link} to="/">
-        Time Management
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        {auth && (
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/timezones">
-              {auth.user.role === UserType.Admin
-                ? "All Timezones"
-                : "Timezones"}
-            </Nav.Link>
+    <>
+      <div className={styles.Header}>
+        <div className={styles.Brand}>
+          <div>buypay</div>
+        </div>
 
-            {(auth.user.role === UserType.Manager ||
-              auth.user.role === UserType.Admin) && (
-                <Nav.Link as={Link} to="/users">
-                  Manage Users
-                </Nav.Link>
-
-              )}
-          </Nav>
-        )}
-
-        <Nav className="ml-auto">
-          {!auth && (
-            <>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/register">
-                Register
-              </Nav.Link>
-            </>
-          )}
+        <div className={styles.ButtonGroup}>
+          <div className={styles.Login}>
+            <div className={styles.LoginText}>
+              Log in
+            </div>
+          </div>
+          <div className={styles.Signup}>
+            <div className={styles.SignupText}>
+              Sign up
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Navbar collapseOnSelect expand="md" variant="dark" bg="primary">
+        <Navbar.Brand as={Link} to="/">
+          buypay
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           {auth && (
-            <NavDropdown title={auth.user.name} id="collasible-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/profile">
-                Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/logout">
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/timezones">
+                {auth.user.role === UserType.Admin
+                  ? "All Timezones"
+                  : "Timezones"}
+              </Nav.Link>
 
-    </Navbar>
+              {(auth.user.role === UserType.Manager ||
+                auth.user.role === UserType.Admin) && (
+                  <Nav.Link as={Link} to="/users">
+                    Manage Users
+                  </Nav.Link>
+
+                )}
+            </Nav>
+          )}
+
+          <Nav className="ml-auto">
+            {!auth && (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
+            {auth && (
+              <NavDropdown title={auth.user.name} id="collasible-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/profile">
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/logout">
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar> */}
+    </>
   );
 };
 
