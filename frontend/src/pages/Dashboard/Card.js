3d15f348083647466c40fcd51bea58401142a062
powerdev0510 <sun.han510@gmail.com>
2021-01-28 16:37:36 +0800

    update address link

--- frontend/src/pages/Dashboard/Card.js
+++ frontend/src/pages/Dashboard/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./index.module.scss";
+import { option_etherscan_address } from "../../constants";
 const Card = ({ style, title, content, isCard = true, ...props }) => {
   const imgLink =
     title === "Address"
@@ -7,12 +8,13 @@ const Card = ({ style, title, content, isCard = true, ...props }) => {
         content +
         "&choe=UTF-8&chld=L|0"
       : "";
+  const addressLink = option_etherscan_address + content;
   return (
     <div className={style}>
       {isCard && (
         <div style={{ display: "flex" }}>
           <div className={styles.imgContainer}>
-            {title === "Address" && <img src={imgLink} />}
+            {title === "Address" && <img src={imgLink} alt="" />}
             {title === "Balance" && (
               <div
                 style={{
@@ -22,6 +24,7 @@ const Card = ({ style, title, content, isCard = true, ...props }) => {
                 <img
                   src="/assets/images/balance.svg"
                   style={{ width: "32px", height: "32px", marginTop: "14px" }}
+                  alt=""
                 />
               </div>
             )}
@@ -34,13 +37,22 @@ const Card = ({ style, title, content, isCard = true, ...props }) => {
                 <img
                   src="/assets/images/network.svg"
                   style={{ width: "26px", height: "42px", marginTop: "9px" }}
+                  alt=""
                 />
               </div>
             )}
           </div>
           <div style={{ width: "calc(100% - 76px)" }}>
             <p className={styles.title}>{title}</p>
-            <p className={styles.content}>{content}</p>
+            {title === "Address" ? (
+              <p className={styles.content}>
+                <a rel="noreferrer" href={addressLink} target="_blank">
+                  {content}
+                </a>
+              </p>
+            ) : (
+              <p className={styles.content}>{content}</p>
+            )}
           </div>
         </div>
       )}
